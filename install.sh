e88f4264fe70de9069019e88e6d403b4a8d578d1
AltioremHominem <116984576+AltioremHominem@users.noreply.github.com>
2024-01-25 15:47:29 -0500

    YES

--- install.sh
+++ install.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 sudo xbps-install -Suy &&
-sudo xbps-install -Sy xorg base-devel git curl wget libXft-devel libX11-devel harfbuzz-devel libXext-devel libXrender-devel libXinerama-devel bspwm sxhkd dunst flameshot htop neovim polybar python jq font-weather-icons font-awesome5 noto-fonts-cjk noto-fonts-emoji picom numlockx hsetroot firefox chrony lxappearance rofi gtk-engine-murrine gtk2-engines font-iosevka void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree xtools zsh &&
-sudo ln -s /etc/sv/chronyd /var/service/ &&
-cp .zshrc ~ &&
-cp .xinitrc ~ &&
-cp .Xresources ~ &&
-cp .p10k.zsh ~ &&
-cp -r .config/ ~ &&
-cp -r .icons/ ~ &&
-cp -r .fonts/ ~ &&
-mkdir ~/.zinit &&
-git clone https://github.com/zdharma/zinit.git ~/.zinit/bin &&
-sudo ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/ &&
-sudo xbps-reconfigure -f fontconfig &&
-sudo fc-cache -f -v &&
-cd ~/Void-Linux-BSPWM/terminal &&
+sudo xbps-install -Sy xorg base-devel git curl wget libXft-devel libX11-devel harfbuzz-devel libXext-devel libXrender-devel libXinerama-devel bspwm sxhkd dunst flameshot htop neovim polybar python jq font-weather-icons font-awesome5 noto-fonts-cjk noto-fonts-emoji picom numlockx hsetroot firefox chrony lxappearance rofi gtk-engine-murrine gtk2-engines font-iosevka void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree xtools zsh 
+sudo ln -s /etc/sv/chronyd /var/service/ 
+cp .zshrc ~ 
+cp .xinitrc ~ 
+cp .Xresources ~ 
+cp .p10k.zsh ~ 
+cp -r .config/ ~ 
+cp -r .icons/ ~ 
+cp -r .fonts/ ~ 
+mkdir ~/.zinit 
+git clone https://github.com/zdharma/zinit.git ~/.zinit/bin 
+sudo ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/ 
+sudo xbps-reconfigure -f fontconfig 
+sudo fc-cache -f -v 
+cd ~/Void-Linux-BSPWM-My-Version-/terminal 
+make 
+sudo make install 
+cd ~/Void-Linux-BSPWM-My-Version-/terminal &&
 make &&
 sudo make install &&
-chmod +x ~/.config/polybar/scripts/* &&
+chmod +x ~/.config/polybar/scripts/* 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' &&
-cd ~/Void-Linux-BSPWM/icon-theme &&
-bash install.sh &&
-cd ~/Void-Linux-BSPWM/theme &&
-bash install.sh &&
-cd ~ &&
-sudo xbps-remove -Oo &&
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' 
+cd ~/Void-Linux-BSPWM-My-Version-/icon-theme 
+bash install.sh 
+cd ~/Void-Linux-BSPWM-My-Version-/theme 
+bash install.sh 
+cd ~ 
+sudo xbps-remove -Oo 
 
 chsh -s /bin/zsh
