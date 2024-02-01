2f2188d1a0e451b498c94a871bde4115ff0c78c4
AltioremHominem <116984576+AltioremHominem@users.noreply.github.com>
2024-02-01 18:59:11 -0500

    Final
    Final

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 sudo xbps-install -Suy &&
-sudo xbps-install -Sy xorg base-devel git curl wget libXft-devel libX11-devel harfbuzz-devel libXext-devel libXrender-devel libXinerama-devel bspwm sxhkd dunst flameshot htop neovim polybar python jq font-weather-icons font-awesome5 noto-fonts-cjk noto-fonts-emoji picom numlockx hsetroot firefox chrony lxappearance rofi gtk-engine-murrine gtk2-engines font-iosevka void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree xtools zsh kitty helix pulseaudio ffmpeg
+sudo xbps-install -Sy xorg base-devel git curl wget libXft-devel libX11-devel harfbuzz-devel libXext-devel libXrender-devel libXinerama-devel bspwm sxhkd dunst flameshot htop neovim polybar python jq font-weather-icons font-awesome5 noto-fonts-cjk noto-fonts-emoji picom numlockx hsetroot firefox chrony lxappearance rofi gtk-engine-murrine gtk2-engines font-iosevka void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree xtools kitty helix pulseaudio ffmpeg betterlockscreen i3lock-color ImageMagick xdpyinfo xrandr xrdb xset llvm
 sudo ln -s /etc/sv/chronyd /var/service/ 
-cp .zshrc ~ 
 cp .xinitrc ~ 
 cp .Xresources ~ 
-cp .p10k.zsh ~ 
 cp -r .config/ ~ 
 cp -r .icons/ ~ 
 cp -r .fonts/ ~ 
@@ -15,13 +13,9 @@ sudo ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
 sudo xbps-reconfigure -f fontconfig 
 sudo fc-cache -f -v 
 chmod +x ~/.config/polybar/scripts/* 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' 
 cd ~/Void-Linux-BSPWM-My-Version-/icon-theme 
 bash install.sh 
 cd ~/Void-Linux-BSPWM-My-Version-/theme 
 bash install.sh 
 cd ~ 
 sudo xbps-remove -Oo 
-
-chsh -s /bin/zsh
